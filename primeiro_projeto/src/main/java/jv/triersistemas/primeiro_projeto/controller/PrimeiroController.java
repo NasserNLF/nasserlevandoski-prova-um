60098103081704e82ffb441465dcd7fa44d7c2f5
Nasser Levandoski <nasserlevandoski@outlook.com>
2024-08-26 10:49:27 -0300

    Primeiro commit

---
+++ primeiro_projeto/src/main/java/jv/triersistemas/primeiro_projeto/controller/PrimeiroController.java
@@ -0,0 +1,57 @@
+package jv.triersistemas.primeiro_projeto.controller;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@RestController
+@RequestMapping("/primeiro-controller")
+public class PrimeiroController {
+	
+	private static List<Produto> produtos = new ArrayList<>();
+	
+	@GetMapping("/texto")
+	public String getText() {
+		return "Hello World!";
+	}
+	
+	@GetMapping("/produto")
+	public Produto getProduto() {
+		return new Produto("Rivotril", BigDecimal.valueOf(15.10));
+	}
+	
+	@GetMapping("/get-all")
+	public List<Produto> getAll(){
+		return produtos;
+	}
+	
+	@PostMapping
+	public void cadastraProduto(@RequestBody Produto produto){
+		produtos.add(produto);
+	}
+	
+}
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+class Produto{
+	
+	private String nome;
+	private BigDecimal preco;
+
+	
+	
+	
+	
+}
